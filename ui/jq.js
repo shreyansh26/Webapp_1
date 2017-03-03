dd40e115e95f431d32b769778aab25243a0bb423
Shreyansh Singh <Shreyansh Singh>
2017-03-03 10:58:05 +0530

    Implemented databases

---
+++ ui/jq.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $('#me').mouseenter(function(){
+      $('#me').fadeTo('fast',0.5)
+    });
+    $('#me').mouseleave(function(){
+      $('#me').fadeTo('fast',1)
+    });
+});
---
+++ ui/main.js
@@ -0,0 +1,77 @@
+console.log('Loaded!');
+
+/*//Change the text of center-text div
+
+var element= document.getElementById('center-text');
+
+element.innerHTML='Hello';
+
+//Move the image
+var img = document.getElementById('madi');
+var marginLeft=0;
+function moveRight(){
+    marginLeft+=2;
+    img.style.marginLeft= marginLeft+'px';
+}
+img.onclick= function(){
+    var interval=setInterval(moveRight,50);
+    //img.style.marginLeft ='100px';
+};*/
+
+
+var button =document.getElementById('counter');
+
+button.onclick = function(){
+    //Create a request object
+    var request=new XMLHttpRequest();
+
+    //Capture Response and store it in a variable
+    request.onreadystatechange =function(){
+      if(request.readyState === XMLHttpRequest.DONE){
+          //Take some action
+          if(request.status===200){
+              var counter=request.responseText;
+              var span=document.getElementById('count');
+              span.innerHTML=counter.toString();
+
+          }
+      }
+    };
+    //Make the request
+    request.open('GET', 'http://localhost:8080/counter',true);
+    request.send(null);
+
+};
+
+
+var submit= document.getElementById('submit_btn');
+
+submit.onclick = function(){
+    //Create a request object
+    var request=new XMLHttpRequest();
+
+    //Capture Response and store it in a variable
+    request.onreadystatechange =function(){
+      if(request.readyState === XMLHttpRequest.DONE){
+          //Take some action
+          if(request.status===200){
+                var names=request.responseText;
+                names= JSON.parse(names);
+                var list='';
+                for(var i=0;i<names.length;i++){
+                    list+='<li>'+ names[i] + '</li>';
+                }
+                var ul = document.getElementById('namelist');
+                ul.innerHTML=list;
+
+          }
+      }
+    };
+    //Submit Name
+    var nameInput= document.getElementById('name');
+    var name=nameInput.value;
+    //Make the request
+    request.open('GET', 'http://localhost:8080/submit-name?name=' + name,true);
+    request.send(null);
+
+};
